ecb54971e9d1658ae5d7650d5b059d8dbff4e04b
Sani Rogers <sanirogers138@gmail.com>
2023-01-08 20:46:43 -0500

    complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const num3 = 29;
+const num4 = 5;
+let multiply;
+let random;
+let mod;
+let max;
+
+multiply = num1 * num2;
+random = Math.floor(Math.random() * 100) + 1;
+mod = num3 % num4;
+max = Math.max(9,20,18,1,2.3,4.5);
